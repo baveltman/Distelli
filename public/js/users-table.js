d846f0afcf69820289bf85336331ac443bfb4c2b
baveltman <baveltman@gmail.com>
2015-04-29 13:22:16 -0500

    Making API call to get user data

--- public/js/users-table.js
+++ public/js/users-table.js
@@ -1,4 +1,33 @@
+var UsersInfo = React.createClass({
+  getInitialState: function() {
+    return {
+      users: '',
+      count: '0'
+    };
+  },
+
+  componentDidMount: function() {
+    $.get(this.props.source, function(results) {
+      var usersInfo = results;
+      if (this.isMounted()) {
+        this.setState({
+          users: usersInfo.users,
+          count: usersInfo.count
+        });
+      }
+    }.bind(this));
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this.state.users}
+      </div>
+    );
+  }
+});
+
 React.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('user-content')
+  <UsersInfo source="https://distelli-baveltman.rhcloud.com/users" />,
+  mountNode
 );
