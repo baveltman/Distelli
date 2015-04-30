523554c91e45bf12a1dfd80b481242d63a8a107c
baveltman <baveltman@gmail.com>
2015-04-29 19:58:24 -0500

    Table complete with pagination

--- public/js/users-table.js
+++ public/js/users-table.js
@@ -4,7 +4,10 @@ var UsersInfo = React.createClass({
   getInitialState: function() {
     return {
       users: '',
-      count: '0'
+      initialCount: 0,
+      count: 0,
+      startIndex: 0,
+      endIndex: 0
     };
   },
 
@@ -14,14 +17,72 @@ var UsersInfo = React.createClass({
       if (this.isMounted()) {
         this.setState({
           users: usersInfo.users,
-          count: usersInfo.count
+          initialCount: usersInfo.count,
+          count: 5,
+          startIndex: 0,
+          endIndex: 5
         });
       }
     }.bind(this));
   },
 
   numUserChange: function(event){
-         this.setState({count: event.target.value});
+  	if (this.state.startIndex + parseInt(event.target.value) <= this.state.initialCount){
+  		//increase page results
+         this.setState(
+         		{
+         			count: parseInt(event.target.value),
+         			endIndex: this.state.startIndex + parseInt(event.target.value) 
+         		});
+     } else {
+     	//show remaining results
+     	this.setState(
+         		{
+         			count: parseInt(event.target.value),
+         			endIndex: this.state.initialCount 
+         		});
+     }
+  },
+
+  pageBack : function(event){
+  	if (this.state.startIndex > 0){
+  		//can page back
+
+  		if (this.state.startIndex - this.state.count >= 0){
+  			this.setState(
+  			{
+  				startIndex: this.state.startIndex - this.state.count,
+  				endIndex: this.state.startIndex,
+  			});
+  		} else {
+  			//show remianing results
+  			this.setState(
+  			{
+  				startIndex: 0,
+  				endIndex: this.state.startIndex
+  			});
+  		}
+  	}
+  },
+
+  pageForward : function(event){
+  	if (this.state.endIndex <= this.state.initialCount){
+  		//can page forward
+  		if (this.state.endIndex + this.state.count <= this.state.initialCount){
+	  		this.setState(
+	  			{
+	  				startIndex: this.state.endIndex,
+	  				endIndex: this.state.endIndex + this.state.count,
+	  			});
+  		} else {
+  			//show remaining results
+  			this.setState(
+	  			{
+	  				startIndex: this.state.endIndex,
+	  				endIndex: this.state.initialCount
+	  			});
+  		}
+  	}
   },
 
   render: function() {
@@ -37,7 +98,7 @@ var UsersInfo = React.createClass({
         var userRows = [];
         var requestedCount = parseInt(this.state.count);
 
-        for (var i=0 ; i < requestedCount; ++i){
+        for (var i=this.state.startIndex ; i < this.state.endIndex; ++i){
         	userRows.push(
                 	<div className="row user-details">
                 		<div className="two columns">{this.state.users[i].first_name}</div>
@@ -53,15 +114,24 @@ var UsersInfo = React.createClass({
         return (
         	<div>
         		<div className="row user-number-results">
-        				<span className="num-results-text"> Number of results: </span>
+        			<div className="four columns">
+        				<span className="num-results-text">Max results Per Page: </span>
 			        	<select id="number-of-users" onChange={this.numUserChange} value={this.state.value}>
-			        		<option value="100">100</option>
-			        		<option value="75">75</option>
-			        		<option value="50">50</option>
-			        		<option value="25">25</option>
-			        		<option value="10">10</option>
 			        		<option value="5">5</option>
+			        		<option value="10">10</option>
+			        		<option value="25">25</option>
+			        		<option value="50">50</option>
+			        		<option value="75">75</option>
+			        		<option value="100">100</option>
 			      		</select>
+			      	</div>
+			      	<div className="eight columns">
+			      		<div className="pager">
+			      			<span className="pager-button" onClick={this.pageBack}> &lt; </span> 
+			      			<span className="pager-button" onClick={this.pageForward}> &gt; </span>
+			      			<span> {this.state.startIndex + 1} </span> - <span> {this.state.endIndex} </span> out of {this.state.initialCount}
+			      		</div>
+			      	</div>
 		      	</div>
 
 	        	<div className="users-table">
